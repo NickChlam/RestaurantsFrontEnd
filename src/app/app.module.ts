2fedf5acfe383f711d7838b1e915c3afd36df75e
NickChlam <Nick.Chlam@rht.comm>
2020-04-12 07:18:51 -0600

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardsComponent } from './cards/cards.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './_services/data.service';
+import { SliderComponent } from './slider/slider.component';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardsComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
